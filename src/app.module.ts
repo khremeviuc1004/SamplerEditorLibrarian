87d5cf1c303de4b25d91a19f14101b10d430a442
Kevin Hremeviuc <kevin.hremeviuc@bizhub.com.au>
2023-05-05 14:50:59 +0930

    Latest client version. Setup for building a single exe for Linux, Windows and Macos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MidiModule } from './midi/midi.module';
   imports: [
     MidiModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../modules/', 'sampler-editor-librarian-client/build'),
+      rootPath: join(__dirname, '../modules/sampler-editor-librarian-client/build'),
     }),
   ],
 })
