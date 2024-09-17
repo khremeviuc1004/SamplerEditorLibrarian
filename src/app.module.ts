73c5293fdf35c157059c523af1d21895c718791a
Kevin Hremeviuc <kevin.hremeviuc@bizhub.com.au>
2024-09-18 09:13:19 +0930

    Added Angular and React build and build configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MidiModule } from './midi/midi.module';
+import { UI_TYPE } from './ui-type';
 
 @Module({
   imports: [
     MidiModule,
     ServeStaticModule.forRoot({
-      rootPath: join(
-        __dirname,
-        '../modules/sampler-editor-librarian-client-angular/dist/sampler-editor-librarian-client-angular/browser/',
-      ),
+      rootPath: join(__dirname, UI_TYPE),
     }),
   ],
 })
